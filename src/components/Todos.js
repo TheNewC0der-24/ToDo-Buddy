e089dbb01d9a196c17db0239ec227ee56f2f9173
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-04-20 21:02:12 +0530

    Create Basic UI for todo application

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Todos = () => {
+
+    const [todo, setTodo] = useState('');
+
+    const handleChange = (e) => {
+        setTodo(e.target.value);
+    }
+    // console.log('ToDo Text: ', todo);
+
     return (
-        <div>Todos</div>
+        <div className='addTodos'>
+            <input
+                type="text"
+                onChange={(e) => handleChange(e)}
+                className='todo-input'
+            />
+            <button className='add-btn'>Add</button>
+        </div>
     )
 }
 
