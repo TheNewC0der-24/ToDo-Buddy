080ca22a1758a770171991a4a6333838a41225e0
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-04-20 21:37:34 +0530

    Connect component with redux store

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,13 +1,27 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { addTodos } from '../redux/reducer';
 
-const Todos = () => {
+const mapStateToProps = (state) => {
+    return {
+        todos: state,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addTodo: (obj) => dispatch(addTodos(obj))
+    };
+};
+
+const Todos = (props) => {
 
     const [todo, setTodo] = useState('');
 
     const handleChange = (e) => {
         setTodo(e.target.value);
     }
-    // console.log('ToDo Text: ', todo);
+    console.log('props from store: ', props);
 
     return (
         <div className='addTodos'>
@@ -21,4 +35,4 @@ const Todos = () => {
     )
 }
 
-export default Todos;
+export default connect(mapStateToProps, mapDispatchToProps)(Todos);
